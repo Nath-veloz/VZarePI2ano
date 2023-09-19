9143e951ba3444a7899596733c9e064e209f8451
Nathan Luz <Nathansluz24@gmail.com>
2023-09-19 13:48:26 -0300

    #

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
 app.listen(9000, () => console. log("OK"));
 
 const mysql = require('mysql2/promise')
@@ -24,4 +26,24 @@ const getAllpessoas = async () => {
 app.get('/pessoa', async (req,res) => {
     const consulta = await getAllpessoas();
     return res.status(200).json(consulta);
+})
+
+app.get('/pessoa/:id', async (req, res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?' , {id});
+    if(query.length === 0) return res.status (400).json({mensagem: ' Nao encontrado.'})
+    return res.status(200).json(query);
+    })
+
+app.get('/pessoa/busca/:nome', async (req, res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?' , {id});
+    if(query.length === 0) return res.status (400).json({mensagem: ' Nao encontrado.'})
+    return res.status(200).json(query);
+    })
+
+app.post('/pessoa', async (req,res)=>{
+    const {nome, email} = req.body;
+    const [query] = await connection.execute('insert into testepessoa.pessoa (nome, email) values(?, ?)', [nome, email])
+    return res.status(200).json(query);
 })
